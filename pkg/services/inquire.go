cbb5cda5f4333dc509c5a06b9c6ed0aa78ba9cac
Hidalgo Edely Chata Ponce <hidalgochponce@gmail.com>
2024-12-01 19:28:37 -0500

    Base de la aplicación creada

---
+++ pkg/services/inquire.go
@@ -0,0 +1,33 @@
+package services
+
+import (
+	"fmt"
+	"github.com/hidalgo27/app-g1/pkg/entities"
+	"github.com/hidalgo27/app-g1/pkg/mappers"
+	"github.com/hidalgo27/app-g1/pkg/models"
+	"github.com/hidalgo27/app-g1/pkg/repositories"
+)
+
+func GetInquire(page, size int) ([]entities.InquireDTO, int64, error) {
+	var inquires []models.Inquire
+
+	var total int64
+	offset := (page - 1) * size
+	total, err := repositories.InquireCount(total)
+
+	fmt.Println(total)
+
+	if err != nil {
+		return nil, 0, err
+	}
+
+	inquires, err = repositories.Inquire(offset, size, inquires)
+	fmt.Println(inquires)
+	if err != nil {
+		return nil, 0, err
+	}
+
+	inquireResponses := mappers.MapInquireDTO(inquires)
+
+	return inquireResponses, total, nil
+}
